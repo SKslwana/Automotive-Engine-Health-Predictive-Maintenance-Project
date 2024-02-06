8a2a407a3af9a58d7321be6fe62534384e657090
SKslwana <97337873+SKslwana@users.noreply.github.com>
2024-02-06 08:37:24 +0200

    Add files via upload

--- Project/app.py
+++ Project/app.py
@@ -5,39 +5,25 @@ model = pickle.load(open('trained_model-1.0.2.pkl', 'rb'))
 
 app = Flask(__name__)
 
-
-
 @app.route('/')
 def man():
     return render_template('home.html')
 
+@app.route('/engine', methods=['GET', 'POST'])
+def engine_post():
+    return render_template('engine.html')
 
-@app.route('/predict', methods=['POST'])
-def home():
-    Engine_rpm = request.form['Engine_rpm']
-    Lub_oil_pressure = request.form['Lub_oil_pressure']
-    Fuel_pressure = request.form['Fuel_pressure']
-    Coolant_pressure = request.form['Coolant_pressure']
-    lub_oil_temp = request.form['lub_oil_temp']
-    Coolant_temp = request.form['Coolant_temp']
-    pred = model.predict([[Engine_rpm, Lub_oil_pressure, Fuel_pressure, Coolant_pressure, lub_oil_temp, Coolant_temp]])[0]
+@app.route('/predict', methods=['GET', 'POST'])
+def predict():
+    Engine_rpm = int(request.form['Engine RPM'])
+    Lub_oil_pressure = float(request.form['Lubrication Oil Pressure'])
+    Fuel_pressure = float(request.form['Fuel Pressure'])
+    Coolant_pressure = float(request.form['Coolant Pressure'])
+    Lub_oil_temp = float(request.form['Lubrication Oil Temperature'])
+    Coolant_temp = float(request.form['Coolant Temperature'])
+    pred = model.predict([[Engine_rpm, Lub_oil_pressure, Fuel_pressure, Coolant_pressure, Lub_oil_temp, Coolant_temp]])
     return render_template('after.html', data=pred)
 
 
 if __name__ == "__main__":
     app.run(debug=True)
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
